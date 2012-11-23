6ea4921f310d06611d0ec20b5a44a9162b73ac3e
Furzel <rdestign+github@gmail.com>
2012-11-23 16:05:37 +0100

    Test files finished + tiny error

--- src/Immediate/Ins_rtaddr.java
+++ src/Immediate/Ins_rtaddr.java
@@ -3,7 +3,7 @@ package Immediate;
 public class Ins_rtaddr extends ImmediateInstruction {
 
 	public static final int[] OP_CODE = { 32, 33, 34, 35, 36, 37, 38, 40, 41, 42, 43, 46, 48, 56 };
-	public static final String[] FUNCTION_NAME = { "lb", "lh", "lwl", "lw", "lbu", "lhu", "lwr", "sb", "sh", "swl", "sw", "swr", "ll", "st" };
+	public static final String[] FUNCTION_NAME = { "lb", "lh", "lwl", "lw", "lbu", "lhu", "lwr", "sb", "sh", "swl", "sw", "swr", "ll", "sc" };
 	
 	public Ins_rtaddr(String binaryString) {
 		super(binaryString);
